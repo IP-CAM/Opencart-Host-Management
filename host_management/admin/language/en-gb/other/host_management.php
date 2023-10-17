9d1a6271f7ac5f0137ad6f294aa722c0bb835119
Mladen Jacimovic <djomlastic@gmail.com>
2023-10-17 03:10:18 +0200

    Adds language terms.

--- host_management/admin/language/en-gb/other/host_management.php
+++ host_management/admin/language/en-gb/other/host_management.php
@@ -40,19 +40,23 @@ $_['button_host_remove' ]  = 'Remove host';
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify security settings!';
 $_['error_warning']        = 'Warning: Please check the form carefully for errors!';
-$_['error_write']          = 'Warning: Could not get write access for: %s file.!';
-$_['error_read']           = '<strong>Error: </strong> Reading default configuration failed.';
-$_['error_protocol']       = 'Invalid %s protocol. Only http and https are allowed.';
-$_['error_hostname']       = 'Invalid %s hostname. Lowercase letters, numbers, hyphens and dots are
+$_['error_read_access']    = 'Warning: Could not get read access for: %s file.!';
+$_['error_write_access']   = 'Warning: Could not get write access for: %s file.!';
+$_['error_read']           = '<strong>Error: </strong> Reading admin config failed. Could not match
+                              protocol, hostname and directory.';
+$_['error_protocol']       = 'Invalid protocol. Only http and https are allowed.';
+$_['error_hostname']       = 'Invalid hostname. Lowercase letters, numbers, hyphens and dots are
                               allowed. It must start and end with leter or number.';
 $_['error_dir']            = 'Invalid %s directory. Letters, numbers, hyphens, underscores and
                               forward slashes are allowed.';
 $_['error_same']           = 'Server and catalog must have same protocol and hostname.';
 $_['error_default_count']  = 'Warning: You must have one default host!';
 $_['error_notice']         = 'Check your admin <i>config.php</i> file syntax.';
+$_['error_update_urls']    = 'Could not match URLs within config file!';
 $_['error_http_server']    = 'Could not match "HTTP_SERVER" definition!';
 $_['error_http_catalog']   = 'Could not match "HTTP_CATALOG" definition!';
 $_['error_php_tag']        = 'Could not match "php" opening tag!';
+$_['error_status']         = 'Status was not changed!';
 
 // Messages
 $_['msg_error_prefix']     = 'Error: ';
