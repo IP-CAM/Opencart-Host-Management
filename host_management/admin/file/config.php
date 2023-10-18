6bb563f4c49143559d705e65aee84090a6f3bd53
Mladen Jacimovic <djomlastic@gmail.com>
2023-10-18 15:39:26 +0200

    Config: bugfixes and refactoring.

--- host_management/admin/file/config.php
+++ host_management/admin/file/config.php
@@ -23,6 +23,16 @@ class Config
     ];
 
     /**
+     * Error message keys for file modes.
+     *
+     * @var array
+     */
+    protected static array $error_keys = [
+        'r' => 'error_read_access',
+        'r+' => 'error_write_access'
+    ];
+
+    /**
      * Log instance.
      *
      * @var Log
@@ -49,6 +59,8 @@ class Config
         $this->messages = $messages;
     }
 
+// #region INTERNAL METHODS
+
     /**
      * Creates file object for given path.
      *
@@ -62,9 +74,33 @@ class Config
             return new SplFileObject($path, $mode);
         } catch (\Throwable $th) {
             $this->log->write(sprintf('%s. Line: %s.', $th->getMessage(), $th->getLine()));
+            $this->messages->error(
+                static::$error_keys[$mode] ?? 'error_file_access',
+                'warning',
+                $path
+            );
         }
 
         return null;
+    }
+
+    /**
+     * Overwrites file contents with given contents.
+     *
+     * @param SplFileObject $file
+     * @param string $content
+     * @return bool
+     */
+    protected function overwrite(SplFileObject $file, string $content): bool
+    {
+        $file->rewind();
+        $file->ftruncate(0);
+
+        $result = $file->fwrite($content);
+
+        $file = null;
+
+        return (bool)$result;
     }
 
     /**
@@ -76,18 +112,20 @@ class Config
     protected function generateUrls(array $hosts): array
     {
         $default = '';
-        $all = '$hm_urls = [' . PHP_EOL;
+        $all = '$hm_urls = [';
 
-        foreach ($hosts as $host) {
+        for ($i = 0; $i < count($hosts); $i++) { 
+            $host = $hosts[$i];
             $url = $host['protocol'] . '://' . $host['hostname'] . '/';
-
-            $all .= '    \'' . $url . '\',' . PHP_EOL;
 
             if (isset($host['default']) && $host['default']) {
                 $default = '$hm_default = \'' . $url . '\'';
             }
-        }
 
+            $all .= ($i !== 0) ? ',' . PHP_EOL : PHP_EOL;
+            $all .= '    \'' . $url . '\'';
+        }
+        
         $all .= PHP_EOL . ']';
 
         return [ 'default' => $default, 'all' => $all ];
@@ -107,9 +145,18 @@ class Config
 
         $code = static::$comments['before'] . PHP_EOL;
         $code .= <<<'EOT'
-        if ((isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on') || ($_SERVER['HTTPS'] == '1'))) || $_SERVER['SERVER_PORT'] == 443) {
+        if (
+            (isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on')
+            || ($_SERVER['HTTPS'] == '1')))
+            || $_SERVER['SERVER_PORT'] == 443
+        ) {
             $hm_protocol = 'https://';
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+        } elseif (
+            !empty($_SERVER['HTTP_X_FORWARDED_PROTO'])
+            && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
+            || !empty($_SERVER['HTTP_X_FORWARDED_SSL'])
+            && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on'
+        ) {
             $hm_protocol = 'https://';
         } else {
             $hm_protocol = 'http://';
@@ -123,7 +170,7 @@ class Config
         EOT;
 
         $code = str_replace([ '*default*', '*urls*' ], [ $urls['default'], $urls['all'] ], $code);
-        $code .= PHP_EOL . PHP_EOL . '// HTTP' . PHP_EOL;
+        $code .= PHP_EOL . PHP_EOL;
         $code .= 'define(\'HTTP_SERVER\', $hm_url';
 
         if ($isAdmin) {
@@ -141,112 +188,29 @@ class Config
     }
 
     /**
-     * Updates URLs within config file contents.
+     * Generates definitions code block
      *
-     * @param string $configStr
-     * @param array $hosts
-     * @return string|null
-     */
-    protected function updateConfigUrls(string $configStr, array $hosts): ?string
-    {
-        $urls = $this->generateUrls($hosts);
-
-        $count = 0;
-
-        $configStr =
-            preg_replace(
-                '/^\$hm_default\s*\=\s*[\"\'][^\'\"]+[\'\"]\s*;\s*$\R/m',
-                $urls['default'],
-                $configStr,
-                1,
-                $count
-            );
-
-        if ($count === 1) {
-            $configStr =
-                preg_replace(
-                    '/^\$hm_urls\s*\=\s*\[[^\]]+\]\s*;\s*$\R/m',
-                    $urls['all'],
-                    $configStr,
-                    1,
-                    $count
-                );
-        }
-
-        if (!$configStr || $count !== 1) {
-            $this->messages->error('error_update_urls');
-
-            return null;
-        }
-
-        return $configStr;
-    }
-
-    /**
-     * Edits config file contents.
-     *
-     * @param string $configStr
-     * @param array $hosts
+     * @param array $host
      * @param array $dirs
-     * @return string|null
+     * @param boolean $isAdmin
+     * @return string
      */
-    protected function editConfig(string $configStr, array $hosts, array $dirs): ?string
+    protected function generateDefinitions(array $host, array $dirs, bool $isAdmin): string
     {
-        $isAdmin =
-            preg_match(
-                '/^define\s*\(\s*[\"\']APPLICATION[\"\']\s*\,\s*[\"\']Admin[\"\']\s*\);\s*$\R/m',
-                $configStr
-            );
-
-        $count = 0;
-
-        $configStr =
-            preg_replace(
-                '/^define\s*\(\s*[\"\']HTTP_SERVER[\'\"]\s*\,\s*[\'\"][^\'\"]+[\'\"]\s*\);\s*$\R/m',
-                '',
-                $configStr,
-                1,
-                $count
-            );
-
-        if (!$configStr || $count !== 1) {
-            $this->messages->error('error_http_server');
-
-            return null;
-        }
+        $definitions = 'define(\'HTTP_SERVER\', \'' . $host['protocol'] . '://';
+        $definitions .= $host['hostname'] . '/' . ($isAdmin ? $dirs['admin'] : $dirs['public']) . '\');';
 
         if ($isAdmin) {
-            $configStr = preg_replace(
-                '/^define\s*\(\s*[\"\']HTTP_CATALOG[\'\"]\s*\,\s*[\'\"][^\'\"]+[\'\"]\s*\);\s*$\R/m',
-                '',
-                $configStr,
-                1,
-                $count
-            );
-
-            if (!$configStr || $count !== 1) {
-                $this->messages->error('error_http_catalog');
-
-                return null;
-            }
+            $definitions .= PHP_EOL . 'define(\'HTTP_CATALOG\', \'' . $host['protocol'] . '://';
+            $definitions .= $host['hostname'] . '/' . $dirs['public'] . '\');';
         }
 
-        $search =
-            preg_match('/^\/\/\s*HTTP\s*$/m', $configStr) ?
-            '/^(\/\/\s*HTTP\s*)$/m'
-            : '/^(<\?(?:php)?)\s*$/m';
-        $replace = '$1' . PHP_EOL . $this->generateCodeBlock($hosts, $dirs, $isAdmin);
-
-        $configStr = preg_replace($search, $replace, $configStr, 1, $count);
-
-        if (!$configStr || $count !== 1) {
-            $this->messages->error('error_php_tag');
-
-            return null;
-        }
-
-        return $configStr;
+        return $definitions;
     }
+
+// #endregion INTERNAL METHODS
+
+// #region PUBLIC INTERFACE
 
     /**
      * Gets error messages.
@@ -268,11 +232,7 @@ class Config
     {
         $file = $this->getFileObj($path);
 
-        if (!$file) {
-            $this->messages->error('error_read_access', 'warning', $path);
-
-            return null;
-        }
+        if (!$file) return null;
 
         $serverPattern =<<<'EOT'
         /define\s*\(
@@ -311,38 +271,172 @@ class Config
     }
 
     /**
-     * Modifies config file.
+     * Edits config file.
      *
      * @param string $path
      * @param array $hosts
      * @param array $dirs
      * @return bool
      */
-    public function modifyConfig(string $path, array $hosts, array $dirs): bool
+    public function edit(string $path, array $hosts, array $dirs): bool
     {
         $file = $this->getFileObj($path,'r+');
 
-        if (!$file) {
-            $this->messages->error('error_write_access', 'warning', $path);
+        if (!$file) return false;
+
+        $content = (string)$file->fread($file->getSize());
+        $count = 0;
+
+        $isAdmin =
+            preg_match(
+                '/^define\s*\(\s*[\"\']APPLICATION[\"\']\s*\,\s*[\"\']Admin[\"\']\s*\);\s*$\R/m',
+                $content
+            );
+
+        $content =
+            preg_replace(
+                '/^define\s*\(\s*[\"\']HTTP_SERVER[\'\"]\s*\,\s*[\'\"][^\'\"]+[\'\"]\s*\);\s*$\R/m',
+                '',
+                $content,
+                1,
+                $count
+            );
+
+        if (!$content || $count !== 1) {
+            $this->messages->error('error_http_server');
+            $file = null;
 
             return false;
         }
 
-        $configStr = $file->fread($file->getSize());
-        $configStr =
-            str_contains($configStr, static::$comments['before']) ?
-            $this->updateConfigUrls($configStr, $hosts) :
-            $this->editConfig($configStr, $hosts, $dirs);
+        if ($isAdmin) {
+            $content = preg_replace(
+                '/^define\s*\(\s*[\"\']HTTP_CATALOG[\'\"]\s*\,\s*[\'\"][^\'\"]+[\'\"]\s*\);\s*$\R/m',
+                '',
+                $content,
+                1,
+                $count
+            );
 
-        if (!$configStr) {
+            if (!$content || $count !== 1) {
+                $this->messages->error('error_http_catalog');
+                $file = null;
+
+                return false;
+            }
+        }
+
+        $search =
+            preg_match('/^\/\/\s*HTTP\s*$/m', $content) ?
+            '/^(\/\/\s*HTTP)\s*$/m'
+            : '/^(<\?(?:php)?)\s*$/m';
+        $replace = '$1' . PHP_EOL . $this->generateCodeBlock($hosts, $dirs, $isAdmin);
+
+        $content = preg_replace($search, $replace, $content, 1, $count);
+
+        if (!$content || $count !== 1) {
+            $this->messages->error('error_php_tag');
+            $file = null;
+
             return false;
         }
 
-        $file->rewind();
-        $file->ftruncate(0);
-        $file->fwrite($configStr);
-        $file = null;
-
-        return true;
+        return $this->overwrite($file, $content);
     }
+
+    /**
+     * Updates config file.
+     *
+     * @param string $path
+     * @param array $hosts
+     * @return boolean
+     */
+    public function update(string $path, array $hosts): bool
+    {
+        $file = $this->getFileObj($path,'r+');
+
+        if (!$file) return false;
+
+        $content = (string)$file->fread($file->getSize());
+        $urls = $this->generateUrls($hosts);
+        $count = 0;
+
+        $content =
+            preg_replace(
+                '/^\$hm_default\s*\=\s*[\"\'][^\'\"]+[\'\"]/m',
+                $urls['default'],
+                $content,
+                1,
+                $count
+            );
+
+        if ($count === 1) {
+            $content =
+                preg_replace(
+                    '/^\$hm_urls\s*\=\s*\[[^\]]+\]/m',
+                    $urls['all'],
+                    $content,
+                    1,
+                    $count
+                );
+        }
+
+        if (!$content || $count !== 1) {
+            $this->messages->error('error_update_urls');
+            $file = null;
+
+            return false;
+        }
+
+        return $this->overwrite($file, $content);
+    }
+
+    /**
+     * Restores config file.
+     *
+     * @param string $path
+     * @param array $host
+     * @param array $dirs
+     * @return bool
+     */
+    public function restore(string $path, array $host, array $dirs): bool
+    {
+        $file = $this->getFileObj($path, 'r+');
+
+        if (!$file) return false;
+
+        $content = (string)$file->fread($file->getSize());
+
+        $isAdmin =
+            preg_match(
+                '/^define\s*\(\s*[\"\']APPLICATION[\"\']\s*\,\s*[\"\']Admin[\"\']\s*\);\s*$\R/m',
+                $content
+            );
+
+        $search =
+            preg_match('/^\/\/\s*HTTP\s*$/m', $content) ?
+            '/^(\/\/\s*HTTP)'
+            : '/^(<\?(?:php)?)';
+        $search .= '\s*$\R^';
+        $search .= str_replace([' ', '/'], ['\s*', '\/'], preg_quote(static::$comments['before']));
+        $search .= '.+';
+        $search .= str_replace([' ', '/'], ['\s*', '\/'], preg_quote(static::$comments['after']));
+        $search .= '$/ms';
+
+        $replace = '$1' . PHP_EOL . $this->generateDefinitions($host, $dirs, $isAdmin);
+
+        $count = 0;
+        $content = preg_replace($search, $replace, $content, -1, $count);
+
+        if (!$content || $count !== 1) {
+            $this->messages->error('error_restore');
+            $file = null;
+
+            return false;
+        }
+
+        return $this->overwrite($file, $content);
+    }
+
+// #endregion PUBLIC INTERFACE
 }
